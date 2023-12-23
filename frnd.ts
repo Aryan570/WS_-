6f6a740268b56198eb34acf214cbb41a85f84c46
Aryan <malhotraaryan570@gmail.com>
2023-12-23 20:28:42 +0530

    Struggling

--- frnd.ts
+++ frnd.ts
@@ -3,13 +3,13 @@ const chats = document.querySelector('.ul');
 const button: HTMLElement | null = document.querySelector('.button')
 // const ip: HTMLElement | null = document.getElementById('ips');
 // console.log(ip)
-const clrs = ['red','cyan','tomato','yellow','lime','blue','violet','hotpink']
-button?.addEventListener('click',()=>{
+const clrs = ['red', 'cyan', 'tomato', 'yellow', 'lime', 'blue', 'violet', 'hotpink']
+button?.addEventListener('click', () => {
     let val = (<HTMLInputElement>document.getElementById('ips'));
     const chat = document.createElement('li')
-    if(val.value){
+    if (val.value) {
         chat.innerHTML = val.value;
-        chat.style.color = clrs[Math.floor((Math.random()*10))%8]
+        chat.style.color = clrs[Math.floor((Math.random() * 10)) % 8]
         chats?.appendChild(chat);
         // console.log(val)
         val.value = "";
@@ -26,9 +26,6 @@ const afterFirstColon = parts.slice(1).join(':');
 // console.log(afterFirstColon);
 const socket = new WebSocket(`ws:${afterFirstColon}`);
 // Connection opened
-socket.addEventListener("open", (event) => {
-  socket.send("Hello Server!");
-});
-socket.addEventListener("close",(e)=>{
-    socket.close()
-})
+// socket.addEventListener("open", (event) => {
+//     socket.send("Hello Server!");
+// });
